19816f8f857eece2668666fc15723c9722b4c7cc
Nidhi0063 <126048657+Nidhi0063@users.noreply.github.com>
2024-01-16 13:34:15 +0530

    Add files via upload
    REST API using spring boot project to post and get data from database in Postgresql.

---
+++ school_api_project-1/src/main/java/school/restapi/project/SchoolApiProject1Application.java
@@ -0,0 +1,13 @@
+package school.restapi.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolApiProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolApiProject1Application.class, args);
+	}
+
+}
---
+++ school_api_project-1/src/test/java/school/restapi/project/SchoolApiProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package school.restapi.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApiProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
